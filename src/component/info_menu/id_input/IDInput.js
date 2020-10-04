e41edd7a6429995092459537d5f5be873d138c0b
Poplar Wang <wang.poplar@gmail.com>
2020-10-04 17:10:10 -0400

    Keypad backend

--- src/component/info_menu/id_input/IDInput.js
+++ src/component/info_menu/id_input/IDInput.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 import Display from './Display';
 import KeyPad from './Keypad';
+import input from '../../../logic/IDInputLogic'
 import './IDInput.css'
 
 export default class IDInput extends React.Component {  
-    handleClick = buttonName => {
-        // TODO!
-    };
+  state = {
+    id: "0",
+    nextItem: "0",
+  };
+
+  handleClick = buttonName => {
+    this.setState(input(this.state, buttonName));
+  };
   
     render() {
       return (
         <div className="component-id-input">
-          <Display value="0" />
+          <Display value={this.state.id} />
           <KeyPad clickHandler={this.handleClick} />
         </div>
       );

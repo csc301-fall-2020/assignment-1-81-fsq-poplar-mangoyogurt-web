8bf900ffa37c5a91109d51a6866d8c26a2faef9f
Poplar Wang <wang.poplar@gmail.com>
2020-10-06 01:52:25 -0400

    Added firebase support

--- src/component/resources/products.js
+++ src/component/resources/products.js
@@ -1,68 +1,19 @@
-var PRODUCTS = {
-  1: {
-    name: 'Poké Ball',
-    price: '200'
-  },
+import firebase from '../../firebase'
 
-  2: {
-    name: 'Great Ball',
-    price: '600'
-  },
+/**
+ * Returns data from Firebase in a JSON form for use by rest of web app
+ */
+var PRODUCTS = {}
 
-  3: {
-    name: 'Ultra Ball',
-    price: '1200'
-  },
-
-  4: {
-    name: 'Potion',
-    price: '300'
-  },
-
-  5: {
-    name: 'Super Potion',
-    price: '700'
-  },
-
-  6: {
-    name: 'Hyper Potion',
-    price: '1200'
-  },
-
-  7: {
-    name: 'Max Potion',
-    price: '2500'
-  },
-
-  8: {
-    name: 'Full Restore',
-    price: '3000'
-  },
-
-  9: {
-    name: 'Revive',
-    price: '1500'
-  },
-
-  10: {
-    name: 'Repel',
-    price: '350'
-  },
-
-  11: {
-    name: 'Super Repel',
-    price: '500'
-  },
-
-  12: {
-    name: 'Max Repel',
-    price: '700'
-  },
-
-  13: {
-    name: 'Escape Rope',
-    price: '550'
-  },
-}
+const productsRef = firebase.database().ref('products')
+productsRef.on('value', (snapshot) => {
+  let dbItems = snapshot.val();
+  for (let item in dbItems) {
+    PRODUCTS[item.toString()] = {
+      name: dbItems[item].name,
+      price: dbItems[item].price
+    }
+  }
+});
 
 export default PRODUCTS
